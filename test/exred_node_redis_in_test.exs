2bbd0298704d21060ad109bfe56dda02b2229731
Zsolt <zsolt.erl@gmail.com>
2019-01-29 16:55:25 -0500

    added tests

--- test/exred_node_redis_in_test.exs
+++ test/exred_node_redis_in_test.exs
@@ -1,4 +1,18 @@
 defmodule Exred.Node.RedisInTest do
   use ExUnit.Case
   doctest Exred.Node.RedisIn
+
+  use Exred.NodeTest, module: Exred.Node.RedisIn
+
+  setup_all do
+    start_node(Exred.Node.RedisDaemon)
+    start_node()
+  end
+
+  test "GET hello", context do
+    Exred.Node.RedisIn.add_out_node(context.pid, self())
+    msg = %{payload: ["hello"]}
+    send(context.pid, msg)
+    assert_receive %{payload: _}, 1000
+  end
 end
